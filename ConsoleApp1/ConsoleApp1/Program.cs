96d846febb09f2d54abf217992bcefa13a6d3fd4
phavryliuk <petrrrusyk93@gmail.com>
2022-10-03 20:44:26 +0300

    123

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Щастя, здоровля, многая лiта");
-Console.ReadKey();  
+Console.WriteLine("Щастя, здоровля, многая лiта 12345");
+Console.ReadKey();
