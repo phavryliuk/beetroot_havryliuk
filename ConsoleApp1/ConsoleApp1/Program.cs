ce8abb3cc2a60c572fe16dacf955ee12a0f15f61
phavryliuk <petrrrusyk93@gmail.com>
2022-10-03 20:39:23 +0300

    +Console read key

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Щастя, здоровля, многая літа");
+Console.WriteLine("Щастя, здоровля, многая лiта");
+Console.ReadKey();  
