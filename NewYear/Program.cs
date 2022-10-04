4cfe5b7d094588954ec427119bd28d89c5e7919a
phavryliuk <petrrrusyk93@gmail.com>
2022-10-04 18:27:22 +0300

    1

---
+++ NewYear/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime lastNY = new DateTime(2022, 1, 1);
+        DateTime nextNY = new DateTime(2023, 1, 1);
+        DateTime today = DateTime.Now;
+
+        
+
+        Console.WriteLine("Вiд попереднього Нового року минуло: " + (today - lastNY) + " днiв");
+        Console.WriteLine("До наступного Нового року залишилося: " + (nextNY - today) + " днiв");
+        Console.ReadKey();
+    } 
+}
